4cb5b936b247415f36c56cc363c47eafefdf00a0
Hlangzti <hlangzti@gmail.com>
2022-06-07 05:37:07 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,21 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
-* print_alphabet - prints alphabet
-*
-*/
+ * print_alphabet - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
 void print_alphabet(void)
 {
-char ch;
+	char alp = 'a';
 
-for (ch = 'a' ; ch <= 'z' ; ch++)
-{
-_putchar(ch);
-}
-_putchar('\n');
+	while (alp <= 'z')
+	{
+		_putchar (alp);
+		alp++;
+
+	}
+	_putchar ('\n');
 
 }
