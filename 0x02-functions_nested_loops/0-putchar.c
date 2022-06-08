569025057f959e52693fd3ebf5fd4b3ea96a166f
Hlangzti <hlangzti@gmail.com>
2022-06-08 02:25:42 -0700

    Task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,11 @@
-#include "hlanga.h"
+#include "main.h"
 /**
- * main - prints the string "hlanga" from a character array.
+ * main - prints the string "main" from a character array.
  * Return: 0 on success.
  */
 int main(void)
 {
-	char c[] = "hlanga";
+	char c[] = "main";
 	int i;
 
 	for (i = 0; i < 9; i++)
