a921cb4b1c317ca42f8d107ff830ae421d397760
Hlangzti <hlangzti@gmail.com>
2022-06-20 06:09:21 -0700

    task 4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,32 +1,19 @@
-#include "holberton.h"
-#include <stdlib.h>
+#include "main.h"
+
 /**
-  *_strpbrk - seraches a string for any of a set of bytes.
-  *it locates the first occurence in the string s of any of
-  *the bytes in the string accept.
-  *@s: pointer to string.
-  *@accept: pointer to strin to be matched.
-  *
-  *Return: pointer to byte in s that matches one of the bytes
-  *in accept ot NULL if no such byte is found.
-  */
+ * _strpbrk - finds first matching char in string
+ * @s: input string to search for matching char
+ * @accept: characters that could be matched
+ * Return: pointer to matching char
+ */
+
 char *_strpbrk(char *s, char *accept)
 {
-	char *p;
-	int counter, counter2;
+	unsigned int i, j;
 
-	p = 0;
-
-	for (counter = 0; s[counter] != '\0'; counter++)
-	{
-		for (counter2 = 0; accept[counter2]; counter2++)
-		{
-			if (accept[counter2] == s[counter])
-			{
-				p = &s[counter];
-				return (p);
-			}
-		}
-	}
-	return (p);
+	for (i = 0; s[i] != '\0'; i++)
+		for (j = 0; accept[j] != '\0'; j++)
+			if (s[i] == accept[j])
+				goto exit;
+exit: return (s[i] != '\0' ? s + i : '\0');
 }
