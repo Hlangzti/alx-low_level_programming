e3abf7f8a5863f6b6f8713250fff4c1326f31273
Hlangzti <hlangzti@gmail.com>
2022-06-20 06:17:32 -0700

    task 8

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,30 +1,18 @@
-#include "holberton.h"
+#include "main.h"
 #include <stdio.h>
+
 /**
-  *print_diagsums - prints sum of two diagonals of a square.
-  *@a: pointer to array.
-  *@size: size of array.
-  *
-  *Return: void.
-  */
+ * print_diagsums - prints sum of #'s in diagnols of square
+ * @a: input square array
+ * @size: size of one dimension in array
+ * Return: void
+ */
+
 void print_diagsums(int *a, int size)
 {
-	int i, size1;
-	unsigned int sumDiag1, sumDiag2;
+	int i, j, sum1 = 0, sum2 = 0;
 
-	size1 = 0;
-	sumDiag1 = 0;
-	sumDiag2 = 0;
-
-	size1 = (size * size) - 1;
-
-	for (i = 0; i <= size1; i = i + (size + 1))
-	{
-		sumDiag1 = sumDiag1 + a[i];
-	}
-	for (i = (size - 1); i < size1; i = i + (size - 1))
-	{
-		sumDiag2 = sumDiag2 + a[i];
-	}
-	printf("%d, %d\n", sumDiag1, sumDiag2);
+	for (j = 0, i = size - 1; j < (size * size); j += size + 1, i += size - 1)
+		sum1 += a[j], sum2 += a[i];
+	printf("%d, %d\n", sum1, sum2);
 }
