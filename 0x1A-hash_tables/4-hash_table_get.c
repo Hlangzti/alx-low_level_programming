fabf6bc16195755a186e27c2099782c1b77c44de
Hlangzti <hlangzti@gmail.com>
2022-09-23 02:41:50 -0700

    hash_table

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,28 +1,27 @@
 #include "hash_tables.h"
+
 /**
- * hash_table_get - retrieves a value associated with a key.
- * @ht: the hash table you want to look into
- * @key: is the key you are looking for
- * Return: value associated with key if found, NULL if failed
- */
+  * hash_table_get - retrieves a value associated with a key.
+  * @ht: pointer to hash table.
+  * @key: key.
+  * Return: value associated with the key, or NULL if key couldn’t be found.
+  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
+	unsigned long int index;
+	hash_node_t *temp_node;
 
-hash_node_t *node;
-unsigned long int index;
+	if (!ht || !key)
+		return (NULL);
 
-if (ht == NULL)
+	/* get index */
+	index = key_index((const unsigned char *)key, ht->size);
+	temp_node = ht->array[index];
+	while (temp_node)
+	{
+		if (strcmp(temp_node->key, key) == 0)
+			return (temp_node->value);
+		temp_node = temp_node->next;
+	}
 	return (NULL);
-
-index = key_index((const unsigned char *) key, ht->size);
-node = ht->array[index];
-
-while (node != NULL)
-{
-	if (strcmp(node->key, key) == 0)
-		return (node->value);
-
-	node = node->next;
-}
-return (NULL);
 }
